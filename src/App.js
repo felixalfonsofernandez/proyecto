a1db84fcddbf5b7defd7d5cdeb02b7cde1320a4c
Cristhel17 <giajahira.quito@unmsm.edu.pe>
2022-05-28 20:05:50 -0500

    Disco Centro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import imagen from '../src/imagenes/logo.png';
+import TareaFormulario from './componentes/TareaFormulario';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+         src={imagen}
+         className='freecodecamp-logo' 
+         alt='el real logo'
+        />
+      </div>
+
+      <div className='tareas-lista-principal'>
+        <h1>Administrador de tareas</h1>
+        <ListaDeTareas />
+      </div>
     </div>
   );
 }
